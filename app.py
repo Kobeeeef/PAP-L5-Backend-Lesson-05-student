2db113645676806e89c9a6a5a417e312d218e5f3
kobel <kobelei335@gmail.com>
2024-10-01 18:31:19 -0700

    done

--- app.py
+++ app.py
@@ -17,7 +17,14 @@
 # Execute the query, pass the results to the display function, 
 # Run this file in terminal, and check the output
 
+import sqlite3
+from visualizer import display
 
+conn = sqlite3.connect('chinook.db')
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM tracks LIMIT 50")
+display(cur)
 
 # Exercise 2
 # Write a SELECT query to the tracks table 
@@ -27,7 +34,12 @@
 # Part 2: Edit the query using the AS keyword to change the column name to "Total Songs"
 # Run the file in terminal, and check your output
 
+cur.execute("SELECT COUNT(TrackId) FROM tracks")
+display(cur)
 
+# Part 2
+cur.execute("SELECT COUNT(TrackId) AS 'Total Songs' FROM tracks")
+display(cur)
 
 # Exercise 3
 # Create a SELECT query to show the values in the Composer column 
@@ -37,8 +49,12 @@
 # Part 2: Edit the SELECT query to count the number of composers
 # Run the file again, and check output
 
+cur.execute("SELECT DISTINCT Composer AS 'Composers' FROM tracks")
+display(cur)
 
-
+# Part 2
+cur.execute("SELECT COUNT(DISTINCT Composer) AS 'Number of Composers' FROM tracks")
+display(cur)
 
 # Exercise 4: Count the number of albums per genre on the tracks table
 # Write a SELECT query to get the GenreId, and COUNT(TrackId) columns 
@@ -47,8 +63,12 @@
 # Part 2: Edit the SELECT query to add Name AS "Example song"
 # Run the file again, and check output
 
+cur.execute("SELECT GenreId, COUNT(TrackId) FROM tracks GROUP BY GenreId;")
+display(cur)
 
-
+# Part 2
+cur.execute("SELECT GenreId, COUNT(TrackId), Name AS 'Example Song' FROM tracks GROUP BY GenreId;")
+display(cur)
 
 # Exercise 5
 # Create a SELECT query to retrieve: 
@@ -58,16 +78,40 @@
 # COUNT TrackId on tracks table AS "Number of songs in genre"
 # GROUP BY tracks.GenreId, execute and pass results to display()
 
-
+cur.execute("""
+SELECT tracks.GenreId, genres.Name AS 'Genre Name', tracks.Name AS 'Example Song', 
+       COUNT(tracks.TrackId) AS 'Number of Songs in Genre'
+FROM tracks
+JOIN genres ON tracks.GenreId = genres.GenreId
+GROUP BY tracks.GenreId;
+""")
+display(cur)
 
 # Exercise 6
 # Copy, paste, and edit the last SELECT query to add a WHERE condition setting genres.GenreId equal to tracks.GenreId
 # Run the file again and check output
 
-
+cur.execute("""
+SELECT tracks.GenreId, genres.Name AS 'Genre Name', tracks.Name AS 'Example Song', 
+       COUNT(tracks.TrackId) AS 'Number of Songs in Genre'
+FROM tracks
+JOIN genres ON tracks.GenreId = genres.GenreId
+WHERE genres.GenreId = tracks.GenreId
+GROUP BY tracks.GenreId;
+""")
+display(cur)
 
 # Bonus
 # Copy, paste and edit the last query
 # Convert it into a more modern form using a JOIN clause
 # View the slides for JOIN clause construction
 # Run the file again and check output
+
+cur.execute("""
+SELECT tracks.GenreId, genres.Name AS 'Genre Name', tracks.Name AS 'Example Song', 
+       COUNT(tracks.TrackId) AS 'Number of Songs in Genre'
+FROM tracks
+INNER JOIN genres ON tracks.GenreId = genres.GenreId
+GROUP BY tracks.GenreId;
+""")
+display(cur)
--- practice.py
+++ practice.py
@@ -6,7 +6,14 @@
 # Use an aggregate function inside a SELECT query to count the number of albums on the tracks table
 # Pass the output to display, run this file in the terminal, and check your results
 
+import sqlite3
+from visualizer import display
 
+conn = sqlite3.connect('chinook.db')
+cur = conn.cursor()
+
+cur.execute("SELECT COUNT(DISTINCT AlbumId) FROM tracks;")
+display(cur)
 
 # Exercise 2
 # Use an aggregate function inside a SELECT query to find the average song length
@@ -14,40 +21,58 @@
 # Milliseconds/60000
 # Pass the output to display, run this file in the terminal, and check your results
 
-
+cur.execute("SELECT AVG(Milliseconds / 60000.0) AS 'Average Length (Minutes)' FROM tracks;")
+display(cur)
 
 # Exercise 3
 # Use an aggregate function inside a SELECT query to find the most expensive song in tracks
 # Pass the output to display, run this file in the terminal, and check your results
 
-
+cur.execute("SELECT MAX(UnitPrice) AS 'Most Expensive Song' FROM tracks;")
+display(cur)
 
 # Exercise 4
 # Use the GROUP BY clause to count the number of albums per genre on the tracks table
 # Execute the query, call display, run the file, and check output
 
+cur.execute("SELECT GenreId, COUNT(AlbumId) AS 'Albums per Genre' FROM tracks GROUP BY GenreId;")
+display(cur)
 
 # Exercise 5
 # Create a SELECT query to count the number of albums per artist 
 # in the albums table
 # Execute the query and pass results to the display function
 
+cur.execute("SELECT ArtistId, COUNT(AlbumId) AS 'Albums per Artist' FROM albums GROUP BY ArtistId;")
+display(cur)
 
 # Exercise 6
 # Recreate the above query to show the artist's name as well
 # You will need to select from both the albums and artists tables, and use a WHERE clause
 # Execute the query and pass results to the display function
 
-
-
-
-
+cur.execute("""
+SELECT artists.Name, COUNT(albums.AlbumId) AS 'Albums per Artist' 
+FROM albums, artists 
+WHERE albums.ArtistId = artists.ArtistId 
+GROUP BY artists.Name;
+""")
+display(cur)
 
 # Exercise 7 - advanced students
 # Run the above query using a JOIN instead of the WHERE clause
 
-
+cur.execute("""
+SELECT artists.Name, COUNT(albums.AlbumId) AS 'Albums per Artist' 
+FROM albums 
+JOIN artists ON albums.ArtistId = artists.ArtistId 
+GROUP BY artists.Name;
+""")
+display(cur)
 
 # Exercise 8
 # Check to make certain all of your queries worked
 # Add a method to close the connection to the database
+
+conn.commit()
+conn.close()
